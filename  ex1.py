b465ba8d67ff6b848452faa85a692d75519b6373
Morgan Williams <you@example.com>
2017-09-18 13:22:05 -0500

    exercise 1

---
+++  ex1.py
@@ -0,0 +1,7 @@
+Print "Hello World!"
+Print "hello again"
+Print "i like typing this."
+Print "this is fun."
+Print 'yay! printing.'
+Print "i'd much rather you 'not'."
+Print 'I "said"do not touch this.'
